6afdbd8b2fa60b8a4e922588931c01ec6058b806
geronimo <geroc120@gmail.com>
2024-05-20 10:37:41 -0300

    se estilizó sección api

--- js/api.js
+++ js/api.js
@@ -17,12 +17,24 @@ function gifAPI(nombre,cant) {
                     <h4 class="animeName">Anime: ${element.anime_name}</h4>
                     <img src="${element.url}">
                 `;
-        
                 containerIMG.append(contenedorCreado);
         
             });
         
     });
 }
+
+
+// idea a futuro: crear un generador de imagenes o gifs en base a la selección del usuario
+// let containerIMG = document.getElementById('container');
+// fetch(`https://api.waifu.pics/sfw/smile`).then((response)=>response.json()).then((datos)=>{
+// console.log(datos.url);
+// const contenedorCreado = document.createElement('div')
+// contenedorCreado.innerHTML=`
+//     <img src ="${datos.url}">
+// `
+// containerIMG.append(contenedorCreado)
+// })
+
 
 
